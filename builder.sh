61a64d4a05660e17b0ed05b175d7ad7f430a1f4a
Jakob <jakob.helgesson@gmail.com>
2025-01-22 21:55:50 +0100

    Refactor builder script and GitHub Actions workflow for yt-dlp. Updated builder script to use new module paths for dependency installation and packaging. Modified GitHub Actions to clone the yt-dlp repository instead of downloading a tarball, improving build efficiency and ensuring the latest code is used.

--- builder.sh
+++ builder.sh
@@ -7,10 +7,10 @@ set -e
 : "${VERSION:=$(date +%Y.%m.%d)}"
 
 source ~/.local/share/pipx/venvs/pyinstaller/bin/activate
-python -m yt_dlp.devscripts.install_deps --include secretstorage --include curl-cffi
-python -m yt_dlp.devscripts.make_lazy_extractors
+python -m devscripts.install_deps --include secretstorage --include curl-cffi
+python -m devscripts.make_lazy_extractors
 python devscripts/update-version.py -c "${CHANNEL}" -r "${ORIGIN}" "${VERSION}"
-python -m yt_dlp.bundle.pyinstaller --onedir
+python -m bundle.pyinstaller --onedir
 deactivate
 
 source ~/.local/share/pipx/venvs/staticx/bin/activate
